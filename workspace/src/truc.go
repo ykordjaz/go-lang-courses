18b3833a25b744ed571a7cd372f78e1fdf0025ae
Yasaman Kordjazy <yasaman.kordjazy@finlex.de>
2023-07-30 20:54:19 +0200

    Coursera: Intro to Golang weeks 1 and 2

---
+++ workspace/src/truc.go
@@ -0,0 +1,11 @@
+/* Program which prompts the user to enter a float which is trunctated to an integer and then printed. */
+
+package main
+
+import "fmt"
+
+func main() {
+	var floatNum float32
+	fmt.Printf("Enter a floating point number: ")
+	fmt.Scan(&floatNum)
+}
